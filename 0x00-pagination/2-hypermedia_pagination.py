6e1b8f9944bb95e94d82c84de6b9535d4ad0515c
hany0ageeb <hany0ageeb@gmail.com>
2024-10-25 10:46:36 +0000

    making changes in 2-hypermedia_pagination.py try fix checker

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -76,7 +76,7 @@ class Server:
     def get_hyper(
             self,
             page: int = 1,
-            page_size: int = 10) -> Dict[str, Union[int, None, List[List]]]:
+            page_size: int = 10) -> Dict:
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset())/page_size)
         """
