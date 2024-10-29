db82076567d10aefc93c3b04d12b5de832d917bd
hany0ageeb <hany0ageeb@gmail.com>
2024-10-29 18:30:37 +0300

    Task 3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,17 +14,24 @@ def put(self, key, item):
     Must assign to the dictionary
     self.cache_data the item value for the key key.
     If key or item is None, this method should not do anything.
+    If the number of items in self.cache_data is
+    higher that BaseCaching.MAX_ITEMS:
+    you must discard the least recently used item (LRU algorithm)
+    you must print DISCARD: with the key discarded and following by a new line
 """
 from base_caching import BaseCaching
+from collections import deque
 
 
 class LRUCache(BaseCaching):
     """LRUCache
     """
+
     def __init__(self):
         """__init__
         """
         super().__init__()
+        self._access = deque(maxlen=BaseCaching.MAX_ITEMS)
 
     def put(self, key, item):
         """
@@ -33,13 +40,21 @@ class LRUCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        pass
-        
+        if key in self.cache_data:
+            self._access.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard_key = self._access.popleft()
+            print('DISCARD: {}'.format(discard_key))
+            del self.cache_data[discard_key]
+        self.cache_data[key] = item
+        self._access.append(key)
 
     def get(self, key):
         """
         return the value in self.cache_data linked to key
         """
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
-        pass
+        self._access.remove(key)
+        self._access.append(key)
+        return self.cache_data[key]
