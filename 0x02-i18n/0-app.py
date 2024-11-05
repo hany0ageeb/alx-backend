9098876013ea1b938e7d601b176c3609aa219a7d
hany0ageeb <hany0ageeb@gmail.com>
2024-11-05 17:55:34 +0200

    Task 0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# 0-app.py
+"""0-app.py
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """index
+    """
+    return render_template('0-index.html')
