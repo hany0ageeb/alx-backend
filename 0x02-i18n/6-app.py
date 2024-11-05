d75661e4dd26c6c9d9c2ef2b3d17a2a7be18b021
hany0ageeb <hany0ageeb@gmail.com>
2024-11-05 20:12:23 +0000

    6-app.py

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -27,7 +27,7 @@ users = {
 @app.before_request
 def before_request() -> None:
     """before request"""
-    setattr(g, 'user', users.get(int(request.arg.get('login_as', 0)), 0))
+    setattr(g, 'user', users.get(int(request.args.get('login_as', 0)), 0))
 
 
 @babel.localeselector
@@ -38,7 +38,7 @@ def get_locale() -> str:
         return locale
     locale = g.user.get('locale', None) if g.user else None
     if locale and locale in Config.LANGUAGES:
-        return local
+        return locale
     locale = request.accept_languages.best_match(app.config['LANGUAGES'])
     if locale and locale in Config.LANGUAGES:
         return locale
