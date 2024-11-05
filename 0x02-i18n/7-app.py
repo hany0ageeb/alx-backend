8fbdd332be254030e0617fd0d781126c16625038
hany0ageeb <hany0ageeb@gmail.com>
2024-11-05 20:25:26 +0000

    7-app.py

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""7-app.py"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union, Dict
+
+
+class Config:
+    """Config"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get_locale"""
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """tmezone"""
+    tz = request.args.get('timezone', '').strip()
+    if not tz and g.user:
+        tz = g.user['timezone']
+    try:
+        return ptyz.timezone(tz).zone
+    except ptyz.exceptions.UnknownTimeZoneError:
+        return app.config['BABEL_DEFAULT_TMEZONE']
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id: str) -> Union[Dict[str, Union[str, None]], None]:
+    """get_user
+    """
+    return users.get(int(id), 0)
+
+
+@app.before_request
+def before_request() -> None:
+    """before request"""
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """index"""
+    return render_template('7-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
