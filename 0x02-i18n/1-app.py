75b7b7f3afd4c2acb7bfa1d362fb43e15f3c41c2
hany0ageeb <hany0ageeb@gmail.com>
2024-11-05 18:27:15 +0200

    Task 1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# 1-app.py
+"""1-app.py
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+babel.init_app(app, default_locale=Config.LANGUAGES[0], default_timezone="UTC")
+
+
+@app.route("/")
+def index():
+    """index
+    """
+    return render_template('1-index.html')
